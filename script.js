b5d883395e2e22e4156889ca9febe55a2e2311c1
Andrey Kulish <andreykulish89@gmail.com>
2022-02-07 12:10:17 +0300

    added sort

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+// ===== находим основные элементы страицы=================================
 let addNewTaskButton = document.querySelector(".head__button");
 let closeTaskButton = document.querySelector(".popup__close");
 let form = document.getElementById("form");
 let popup = document.querySelector(".popup");
 let message = document.querySelector(".head__message");
+let sort = document.querySelector(".sort");
+let select = document.querySelector(".sort__select");
 
-if (localStorage.getItem("data") == null) {
-  localStorage.setItem("data", JSON.stringify([]));
-}
+// ======устанавливаем Storage и рендерим страницу=====================
+setStorage();
 render();
+
+// ====Вешаем события на кнопки, форму и select =======================
 
 addNewTaskButton.addEventListener("click", function (e) {
   popup.classList.add("open");
@@ -31,6 +35,17 @@ form.addEventListener("submit", function (e) {
   let newObj = {};
   for (let [name, value] of formdata) {
     newObj[name] = value;
+    switch (value) {
+      case "Critical":
+        newObj.priority = 3;
+        break;
+      case "Normal":
+        newObj.priority = 2;
+        break;
+      case "Minor":
+        newObj.priority = 1;
+        break;
+    }
   }
   creatNewData(newObj);
 
@@ -41,11 +56,22 @@ form.addEventListener("submit", function (e) {
   e.preventDefault();
 });
 
+select.addEventListener("change", function (e) {
+  localStorage.setItem("sort", select.value);
+  render();
+});
+
+// ===== функции работы с данными=================================
+
+// ========добавляем новый объект в Localstorage, в массив data======
 function creatNewData(obj) {
   let currentData = JSON.parse(localStorage.getItem("data"));
   currentData.push(obj);
   localStorage.setItem("data", JSON.stringify(currentData));
 }
+//
+//
+//====== функция-обработчик события: удаление задачи из списка======================
 function deleteTask(but) {
   let answer = confirm("Are you sure?");
   if (answer) {
@@ -53,17 +79,23 @@ function deleteTask(but) {
     let newData = JSON.parse(localStorage.getItem("data")).filter((elem) => {
       return elem.name !== objectName;
     });
-    console.log(newData);
     localStorage.setItem("data", JSON.stringify(newData));
-
     render();
   }
 }
+// =====render - сбор, сортировка данных с Localstorage, рендер всей станицы: задач, главного сообщения и окна сотировки=====================
 function render() {
   let container = document.querySelector(".main__tasks");
   let data = JSON.parse(localStorage.getItem("data"));
+  let sortSelector = localStorage.getItem("sort");
+  let sortedData;
+  if (sortSelector == "priority") {
+    sortedData = data.sort((obj1, obj2) => obj2.priority - obj1.priority);
+  } else if (sortSelector == "name") {
+    sortedData = data.sort((obj1, obj2) => obj2.name - obj1.name);
+  }
   let content = "";
-  data.forEach((task) => {
+  sortedData.forEach((task) => {
     content += createHTMLTask(task);
   });
   container.innerHTML = content;
@@ -74,8 +106,12 @@ function render() {
       e.preventDefault();
     });
   }
-  editMessage(data);
+  editMessage(sortedData);
+  toggleSortContainer(sortedData);
 }
+//
+
+// ===========функция создает HTML разметку для каждой Task.
 function createHTMLTask(object) {
   return `<div class="task"><h3 class="task__name">${object.name}</h3><p class="task__description">${object.description}</p><div class="task__priority">
   <div class="task-priority-left">
@@ -92,6 +128,9 @@ function createHTMLTask(object) {
 </div>
 </div>`;
 }
+//
+//
+// ==========функция меняет текст главного собщения на странице в зависимости от Data
 function editMessage(data) {
   if (data.length == 0) {
     message.innerHTML = "No tasks today! Don't worry";
@@ -99,5 +138,26 @@ function editMessage(data) {
     message.innerHTML = `You've got <span style="color:#F3477A">${data.length} task</span> today!`;
   } else {
     message.innerHTML = `You've got <span style="color:#F3477A">${data.length} tasks</span> today!`;
+  }
+}
+//
+//
+// ============= включение и выключения окна сортировки в зависимости от количества задча=====================================================================
+function toggleSortContainer(data) {
+  if (data.length <= 1) {
+    sort.classList.add("none");
+  } else {
+    sort.classList.remove("none");
+  }
+}
+//
+//
+// =======установка начального storage===================================
+function setStorage() {
+  if (localStorage.getItem("data") == null) {
+    localStorage.setItem("data", JSON.stringify([]));
+  }
+  if (localStorage.getItem("sort") == null) {
+    localStorage.setItem("sort", select.value);
   }
 }
