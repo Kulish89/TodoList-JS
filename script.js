dd8271ab86a152ae34e63128ded626b38c97b467
Andrey Kulish <andreykulish89@gmail.com>
2022-02-14 12:10:33 +0300

    validation

--- script.js
+++ script.js
@@ -6,21 +6,29 @@ let popup = document.querySelector(".popup");
 let message = document.querySelector(".head__message");
 let sort = document.querySelector(".sort");
 let select = document.querySelector(".sort__select");
-const PRIORITY = { Critical: 3, Normal: 2, Minor: 1 };
+let reqTextarea = document.querySelector(".form__textarea");
+let reqInput = document.querySelector(".form__input");
 
 //
-localStorage.clear();
+const PRIORITY = { Critical: 3, Normal: 2, Minor: 1 };
+//
+
+//
+//
 setStorage(select.value);
 render();
 
 //
-
+//
+//
+//
+// AddEventListeners!======================================================
 addNewTaskButton.addEventListener("click", function (e) {
   popup.classList.add("open");
   popup.addEventListener("click", function (e) {
     if (!e.target.closest(".popup__content")) {
       popup.classList.remove("open");
-      form.reset();
+      resetForm();
     }
   });
   e.preventDefault();
@@ -28,27 +36,37 @@ addNewTaskButton.addEventListener("click", function (e) {
 
 closeTaskButton.addEventListener("click", function (e) {
   popup.classList.remove("open");
-  form.reset();
+  resetForm();
   e.preventDefault();
 });
 
 form.addEventListener("submit", function (e) {
-  let formdata = new FormData(form);
-  let newObj = {};
-  newObj.id = Date.now();
-  for (let [name, value] of formdata) {
-    newObj[name] = value;
-    if (name === "prioritySelector") {
-      newObj.priority = PRIORITY[value];
-    }
-  }
-  creatNewData(newObj);
-
-  render();
-
-  popup.classList.remove("open");
-  form.reset();
   e.preventDefault();
+  let requairedElements = document.querySelectorAll("._req");
+  let error = validation(...requairedElements);
+  if (error === 0) {
+    let formdata = new FormData(form);
+    let newObj = {};
+    newObj.id = Date.now();
+    for (let [name, value] of formdata) {
+      newObj[name] = value;
+      if (name === "prioritySelector") {
+        newObj.priority = PRIORITY[value];
+      }
+    }
+    creatNewData(newObj);
+    popup.classList.remove("open");
+    resetForm();
+    render();
+  }
+});
+
+reqTextarea.addEventListener("input", function (e) {
+  validation(reqTextarea);
+});
+
+reqInput.addEventListener("input", function (e) {
+  validation(reqInput);
 });
 
 select.addEventListener("change", function (e) {
@@ -57,16 +75,16 @@ select.addEventListener("change", function (e) {
 });
 
 //
-
+//
+//
+// ====Functions!===========================
 //
 function creatNewData(obj) {
   let currentData = JSON.parse(localStorage.getItem("data"));
   currentData.push(obj);
   localStorage.setItem("data", JSON.stringify(currentData));
 }
-//
-//
-//
+
 function deleteTask(id) {
   let answer = confirm("Are you sure?");
   if (answer) {
@@ -77,8 +95,6 @@ function deleteTask(id) {
     render();
   }
 }
-//
-//
 
 function render() {
   let container = document.querySelector(".main__tasks");
@@ -93,9 +109,7 @@ function render() {
   editMessage(sortedData.length);
   showSortContainer(sortedData.length);
 }
-//
 
-// ===========
 function createHTMLTask(object) {
   return `<div class="task" id = "${object.id}"><h3 class="task__name">${object.name}</h3><p class="task__description">${object.description}</p><div class="task__priority">
   <div class="task-priority-left">
@@ -112,9 +126,7 @@ function createHTMLTask(object) {
 </div>
 </div>`;
 }
-//
-//
-//
+
 function editMessage(length) {
   if (length == 0) {
     message.innerHTML = "No tasks today! Don't worry";
@@ -124,9 +136,7 @@ function editMessage(length) {
     message.innerHTML = `You've got <span style="color:#F3477A">${length} tasks</span> today!`;
   }
 }
-//
-//
-//
+
 function showSortContainer(length) {
   if (length <= 1) {
     sort.classList.add("none");
@@ -134,9 +144,7 @@ function showSortContainer(length) {
     sort.classList.remove("none");
   }
 }
-//
-//
-// =======Set Storage==================================
+
 function setStorage(value) {
   if (localStorage.getItem("data") == null) {
     localStorage.setItem("data", JSON.stringify([]));
@@ -145,7 +153,6 @@ function setStorage(value) {
     localStorage.setItem("sort", value);
   }
 }
-// ===========Sort data================
 
 function sortData(data, selector) {
   return data.sort(function (obj1, obj2) {
@@ -161,4 +168,37 @@ function sortData(data, selector) {
       return obj2[selector] - obj1[selector];
     }
   });
+}
+
+function validation(...args) {
+  let error = 0;
+  for (let reqElem of args) {
+    if (reqElem.name == "name" && reqElem.value.length > 20) {
+      reqElem.classList.add("error");
+      reqElem.nextElementSibling.classList.add("active");
+      reqElem.nextElementSibling.innerText = `Your name is too long!`;
+      error++;
+    } else if (reqElem.validity.valueMissing) {
+      reqElem.classList.add("error");
+      reqElem.nextElementSibling.classList.add("active");
+      reqElem.nextElementSibling.innerText = `You need to enter ${reqElem.name}!`;
+      error++;
+    } else {
+      reqElem.classList.remove("error");
+      reqElem.nextElementSibling.innerText = "";
+      reqElem.nextElementSibling.classList.remove("active");
+    }
+  }
+
+  return error;
+}
+
+function resetForm() {
+  let requairedElements = document.querySelectorAll("._req");
+  for (let reqElem of requairedElements) {
+    reqElem.classList.remove("error");
+    reqElem.nextElementSibling.innerText = "";
+    reqElem.nextElementSibling.classList.remove("active");
+  }
+  form.reset();
 }
